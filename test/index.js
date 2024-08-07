ed9b7aa80b55b0eab471bea9c36ed44b70bcd25b
Dan Sargeant <dan@keywordintent.com>
2024-08-07 14:04:35 +1000

    fix: Pass sourcemap as parameter to minify

--- test/index.js
+++ test/index.js
@@ -11,7 +11,22 @@ describe('LessPluginCleanCSS', function () {
 
         return less.render(lessString, { plugins: [cleanCSSPlugin] })
             .then(function (result) {
-                assert(result.css === 'body{color:red}');
+                assert.equal(result.css, 'body{color:red}');
+            });
+    });
+
+    it('should generate sourcemaps', function () {
+        const less = require('less');
+        const LessPluginCleanCSS = require('..');
+
+        const cleanCSSPlugin = new LessPluginCleanCSS({advanced: true});
+
+        const lessString = 'body { color: #f00; }';
+
+        return less.render(lessString, { plugins: [cleanCSSPlugin], sourceMap: { outputSourceFiles: true } })
+            .then(function (result) {
+                assert.equal(result.css,'body{color:red}');
+                assert.equal(result.map, '{"version":3,"sources":["input"],"names":[],"mappings":"AAAA,KAAO,MAAA","sourcesContent":["body { color: #f00; }"]}');
             });
     });
 });
